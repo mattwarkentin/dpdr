351db0d55944f741bf421ce6280a1b46f415dfcc
Matthew T. Warkentin <warkentin@lunenfeld.ca>
2023-12-01 12:00:38 -0700

    Initial commit

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# dpdr 0.0.0.9000
+
+* Under development.
---
+++ R/Utils.R
@@ -0,0 +1,8 @@
+#' Base URL for Health Canada DPD
+#'
+#' @return String.
+#'
+#' @export
+api_base_url <- function() {
+  'https://health-products.canada.ca/api/'
+}
